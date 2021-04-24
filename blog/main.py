cb1cc6dcf6ff7109434855ceee12169af617e6ed
rodriguesms <miguelrodrigues@eng.ci.ufpb.br>
2021-04-24 02:03:58 -0300

    Criando funçao de inserir blog no banco, listar todos os blogs e consultar blog especifico por id

--- blog/main.py
+++ blog/main.py
@@ -1,11 +1,36 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from . import schemas, models
-from .database import engine
+from .database import engine, SessionLocal
+from sqlalchemy.orm import Session
+
 app = FastAPI()
 
 models.Base.metadata.create_all(engine)
 
-@app.post('/blog')
-def create(request: schemas.Blog):
-    return request
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
+
+@app.post('/blog')
+def CreateBlog(request: schemas.Blog, db: Session = Depends(get_db)):
+    new_blog = models.Blog(title=request.title, body=request.body)
+    db.add(new_blog)
+    db.commit()
+    db.refresh(new_blog)
+    return new_blog
+
+
+@app.get('/blog')
+def ListBlogs(db: Session = Depends(get_db)):
+    blogs = db.query(models.Blog).all()
+    return blogs
+
+
+@app.get('/blog/{id}')
+def ShowBlog(id, db: Session = Depends(get_db)):
+    blog = db.query(models.Blog).filter(models.Blog.id == id).first()
+    return blog
